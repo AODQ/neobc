aff24333b05ce5ea117eefc5213373911857874f
aodq <dtoadq@gmail.com>
2019-08-09 00:00:20 -0400

    ToString works on enums, integrals & floating

--- source/neobc/string.d
+++ source/neobc/string.d
@@ -5,9 +5,37 @@ import neobc.array;
 import core.stdc.stdlib;
 import core.stdc.string;
 
-string ToString(T)(T value) {
+string ToStringEnum(T)(T value) {
   immutable static string[] mem = [__traits(allMembers, T)];
   return mem[cast(size_t)(value)];
+}
+
+String ToString(T)(T value) if (__traits(isIntegral, T)) {
+  import core.stdc.stdlib;
+  import core.stdc.stdio;
+  import core.stdc.math;
+
+  T logValue = value < 0 ? -value : value;
+  if (logValue == 0) logValue = 1;
+
+  String s;
+  s.Resize(cast(int)(floor(log10(logValue)+1)));
+  snprintf(s.ptr, s.length, "%d", value);
+  return s;
+}
+
+String ToString(T)(T value) if (__traits(isFloating, T)) {
+  import core.stdc.stdlib;
+  import core.stdc.math;
+
+  int integral = cast(int)value;
+  int floating = cast(int)((value - cast(float)integral) * 100.0f);
+
+  String s;
+  s ~= ToString(integral);
+  s ~= ".";
+  s ~= ToString(floating);
+  return s;
 }
 
 struct String {
