a9d006155519786bbfda0cc9390df096e31b0220
aodq <dtoadq@gmail.com>
2019-08-09 03:12:25 -0400

    implements basic swap function

--- source/neobc/memory.d
+++ source/neobc/memory.d
@@ -4,3 +4,15 @@ T* Allocate(T)(size_t count = 1) {
   import core.stdc.stdlib;
   return cast(T*)calloc(count, T.sizeof * count);
 }
+
+void Swap(T)(ref T left, ref T right) if (__traits(isIntegral, T)) {
+  left  ^= right;
+  right ^= left;
+  left  ^= right;
+}
+
+void Swap(T)(ref T* left, ref T* right) if (!__traits(isIntegral, T)) {
+  T* tmp = left;
+  left   = right;
+  right  = tmp;
+}
