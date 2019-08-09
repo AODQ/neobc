83cea0859978f3dee7dba316b52828adca7b5fea
aodq <dtoadq@gmail.com>
2019-08-09 03:12:51 -0400

    Adds thread sleep function

--- source/neobc/time.d
+++ source/neobc/time.d
@@ -8,3 +8,13 @@ uint64_t GetNsTime() {
     return clock() / (CLOCKS_PER_SEC / 1_000_000);
   }
 }
+
+void Sleep() {
+  import core.sys.posix.time;
+
+  timespec requested, remainder;
+  requested.tv_nsec = 5;
+  requested.tv_sec = 0;
+
+  nanosleep(&requested, &remainder);
+}
